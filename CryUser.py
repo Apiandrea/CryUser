56d46d86b9ca84ee365cf9b92fe6473144017672
Apiandrea <apinadrea@gmail.com>
2024-01-02 19:41:07 +0100

    Apiandrea/ first version, now CryUser can enrypt and decrypt a file with AES-CBC cryptosystem, using tkinter

---
+++ CryUser.py
@@ -0,0 +1,74 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+import tkinter as tk
+import os
+import datetime
+from PIL import Image, ImageTk
+from tkinter import filedialog as fd
+
+window = tk.Tk()
+window.geometry("300x450")
+window.title("  CryUser -- By Apiandrea")
+window.configure(background="gray")
+window.resizable(False, False)
+
+intro = tk.Label(text = "CHOOSE THE SYSTEM", font=("Helvetica", 16))
+intro.grid(column = 0, row = 0, padx = 75, pady=20)
+
+# SNIPPET FOR SELECTING THE FILE
+def select_file():
+    window.filename = fd.askopenfilename(title = "Choose the file", initialdir="/home")
+open_button = tk.Button(window, text="Choose the file", command=select_file)
+open_button.grid(column=0, row=1, pady=10)
+
+def AES_clicked():
+    print(window.filename)
+    intro.destroy()
+    introduce = tk.Label(text = "CHOOSE THE MODE", font=("Helvetica", 16))
+    introduce.grid(column = 0, row = 0, padx = 75, pady=20)
+    AES_button.destroy()
+    RSA_button.destroy()
+
+    def ENC_clicked():
+        with open(window.filename, "rb") as f:
+            data = f.read()
+        key = get_random_bytes(16)
+        print(key)
+
+        with open(window.filename + "_aeskey.txt", "wb") as f:
+            f.write(key)
+
+        cipher = AES.new(key, AES.MODE_CBC)
+        e_data = cipher.encrypt(pad(data, AES.block_size))
+
+        with open(window.filename, "wb") as f:
+            f.write(cipher.iv)
+            f.write(e_data)
+
+    def DEC_clicked():
+        with open(window.filename, "rb") as f:
+            iv = f.read(16)
+            data = f.read()
+        key_name = window.filename + "_aeskey.txt"
+        with open(key_name, "rb") as f:
+            key = f.read(16)
+        dec = AES.new(key, AES.MODE_CBC, iv=iv)
+        decrypted_data = unpad(dec.decrypt(data), AES.block_size)
+
+        with open(window.filename, "wb") as f:
+            f.write(decrypted_data)
+
+    ENC_button = tk.Button(window, text="ENC", bg = "red", command = ENC_clicked, font=("Helvetica", 16))
+    ENC_button.grid(column=0, row=2)
+    DEC_button = tk.Button(window, text="DEC", bg = "red", command = DEC_clicked, font=("Helvetica", 16))
+    DEC_button.grid(column=0, row=3)
+
+AES_button = tk.Button(window, text="AES",bg = "red", command=AES_clicked, font=("Helvetica", 16))
+AES_button.grid(column=0, row=2)
+
+RSA_button = tk.Button(window, text="RSA", bg = "red", font=("Helvetica", 16))
+RSA_button.grid(column=0, row=3)
+
+
+window.mainloop()
